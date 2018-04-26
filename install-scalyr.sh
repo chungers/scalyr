efcf39150b5dc696b0a9d6a05e8ae8475ba9dafc
David Chung <david.chung@docker.com>
2018-04-25 18:06:13 -0700

    add -y
    Signed-off-by: David Chung <david.chung@docker.com>

--- install-scalyr.sh
+++ install-scalyr.sh
@@ -4,7 +4,7 @@
 
 echo "Installing on Scalyr: note this is RHEL only at the moment"
 
-sudo yum install wget
+sudo yum install -y wget
 wget -q https://www.scalyr.com/scalyr-repo/stable/latest/install-scalyr-agent-2.sh
 sudo bash ./install-scalyr-agent-2.sh --set-api-key "{{$apiKey}}" --start-agent
 
