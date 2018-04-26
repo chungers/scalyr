fcfcd12c476ce1f40ac702ffee8f01167a28ecd7
David Chung <david.chung@docker.com>
2018-04-26 11:30:12 -0700

    fix install of agent.json file
    Signed-off-by: David Chung <david.chung@docker.com>

--- install-scalyr.sh
+++ install-scalyr.sh
@@ -2,10 +2,8 @@
 
 $(sudo scalyr-agent-2 stop)
 
-{{ $apiKey := flag "scalyr-api-key" "string" "api key" | prompt "Scalyr API key?" "string" "0ySRtj7FVbL1CPxQFcP4Qna0yrpVJYWo7bXwfzSE3Jcw-" }}
 
-{{ var `apiKey` $apiKey}}{{/* sets a variable for sourced template */}}
-echo <<EOF > /tmp/agent.json
+cat <<EOF > /tmp/agent.json
 {{ source `agent.json` }}
 EOF
 mkdir -p /etc/scalyr-agent-2
@@ -15,6 +13,6 @@ echo "Installing on Scalyr: note this is RHEL only at the moment"
 
 sudo yum install -y wget
 wget -q https://www.scalyr.com/scalyr-repo/stable/latest/install-scalyr-agent-2.sh
-sudo bash ./install-scalyr-agent-2.sh --set-api-key "{{$apiKey}}" --start-agent
+sudo bash ./install-scalyr-agent-2.sh --set-api-key "{{ var `apiKey` }}" --start-agent
 
 sudo scalyr-agent-2 start
