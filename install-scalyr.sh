aefaee0061df7eee4c18736ce78ad8344796d8fe
David Chung <david.chung@docker.com>
2018-04-25 18:24:05 -0700

    add flags
    Signed-off-by: David Chung <david.chung@docker.com>

--- install-scalyr.sh
+++ install-scalyr.sh
@@ -1,6 +1,6 @@
 {{/* =% sh %= */}}
 
-{{ $apiKey := flag "api-key" "string" "api key" | prompt "API key?" "string" "0ySRtj7FVbL1CPxQFcP4Qna0yrpVJYWo7bXwfzSE3Jcw-" }}
+{{ $apiKey := flag "scalyr-api-key" "string" "api key" | prompt "Scalyr API key?" "string" "0ySRtj7FVbL1CPxQFcP4Qna0yrpVJYWo7bXwfzSE3Jcw-" }}
 
 echo "Installing on Scalyr: note this is RHEL only at the moment"
 
@@ -9,8 +9,9 @@ wget -q https://www.scalyr.com/scalyr-repo/stable/latest/install-scalyr-agent-2.
 sudo bash ./install-scalyr-agent-2.sh --set-api-key "{{$apiKey}}" --start-agent
 
 {{ var `apiKey` $apiKey}}{{/* sets a variable for sourced template */}}
-echo <<EOF > /etc/scalyr/agent.json
+echo <<EOF > /tmp/agent.json
 {{ source `agent.json` }}
 EOF
 
+sudo cp /tmp/agent.json /etc/scalyr/agent.json
 sudo scalyr-agent-2 start
