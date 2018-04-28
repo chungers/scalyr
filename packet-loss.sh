2a5dacfaaff2048e10f6adfc0ed1104f6a0a98be
David Chung <david.chung@docker.com>
2018-04-28 10:08:19 -0700

    packet loss
    Signed-off-by: David Chung <david.chung@docker.com>

---
+++ packet-loss.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+{{/* =% sh %= */}}
+
+{{ flag "ip1" "string" "IP1" | prompt "IP1?" "string" "172.31.30.198" | var `ip1`}}
+{{ flag "ip2" "string" "IP2" | prompt "IP2?" "string" "172.31.31.86" | var `ip2`}}
+{{ flag "ip3" "string" "IP3" | prompt "IP3?" "string" "172.31.17.169" | var `ip3`}}
+{{ flag "loss" "int" "% loss" | prompt "% Packet loss?" "int" 50 | var `loss`}}
+{{ flag "delay" "int" "msec delay" | prompt "Delay in msec" "int" 100 | var `delay`}}
+
+interface=eth0
+ip1={{ var `ip1` }}
+ip2={{ var `ip2` }}
+ip3={{ var `ip3` }}
+
+delay={{- var `delay` -}}ms
+loss={{- var `loss` -}}%
+
+tc qdisc add dev $interface root handle 1: prio
+tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $ip1 flowid 2:1
+tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $ip2 flowid 2:1
+tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $ip3 flowid 2:1
+#tc qdisc add dev $interface parent 1:1 handle 2: netem delay $delay
+tc qdisc add dev $interface parent 1:1 handle 2: netem loss $loss
